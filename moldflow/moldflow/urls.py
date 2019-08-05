721f2941061b619cde3e9fda9cb2ea626211b076
Aerial <matas234@gmail.com>
2019-08-05 20:04:57 +0200

    Add admin site descriptors
    Including site_header, site_title, index_title

--- moldflow/moldflow/urls.py
+++ moldflow/moldflow/urls.py
@@ -11,4 +11,7 @@ urlpatterns = [
     ),  # add login to api
 ]
 
+admin.site.site_header = '***REMOVED*** on Demand administration'
+admin.site.site_title = '***REMOVED***'
+admin.site.index_title = 'Administration'
 urlpatterns += staticfiles_urlpatterns()  # static files for gunicorn
